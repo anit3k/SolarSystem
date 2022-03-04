875d990c7ad10f0d8a8cbfc5b471eaf84093676b
luczassz <szabszika00@gmail.com>
2022-03-04 09:29:16 +0100

    valami

---
+++ Static Html Lib/szabolcs/Stardust/stardust.js
@@ -0,0 +1,9 @@
+var frame = document.getElementById("stardust-container");
+var maxStar = Math.random()
+
+for (var starcounter = 0; starcounter < array.length; starcounter++) {
+    const star = document.createElement("span");
+    star.appendChild(textnode);
+    document.getElementById("stardust-container").appendChild(star);
+    
+}
