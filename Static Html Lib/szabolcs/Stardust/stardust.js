e3e0c475182ee3353f272f5bf5efa9c01fe996e9
luczassz <szabszika00@gmail.com>
2022-03-07 12:34:34 +0100

    valami

--- Static Html Lib/szabolcs/Stardust/stardust.js
+++ Static Html Lib/szabolcs/Stardust/stardust.js
@@ -1,7 +1,7 @@
 //Generate a random number between 1000 and 1200 for the stars
 var maxStar = Math.floor( Math.random() * 1200 )+ 1000;
 
-//Generate random amount of stars
+//Generate random amount of stars with random positions
 for (var starcounter = 0; starcounter < maxStar; starcounter++) {
     var positionX = Math.floor( Math.random() * (window.innerWidth - 15) );
     var positionY = Math.floor( Math.random() * (window.innerHeight - 15) );
@@ -12,17 +12,58 @@ for (var starcounter = 0; starcounter < maxStar; starcounter++) {
     
 }
 
-function mouse_position()
-{
-    var e = window.event;
+//Getting the position of the mouse
 
-    var posX = e.clientX;
-    var posY = e.clientY;
-
-    console.log(posX);
-    console.log(posY);
-
-    setTimeout(mouse_position,100);
+var x = null;
+var y = null;
     
+document.addEventListener('mousemove', onMouseUpdate, false);
+document.addEventListener('mouseenter', onMouseUpdate, false);
+    
+function onMouseUpdate(e) {
+  x = e.pageX;
+  y = e.pageY;
+  console.log(x, y);
 }
-mouse_position();
+
+function getMouseX() {
+  return x;
+}
+
+function getMouseY() {
+  return y;
+}
+
+//Making the stars move
+
+const element = document.getElementById("stardust-container");
+setInterval(function() {
+    var stars = document.getElementsByTagName("span");
+
+ 
+
+    for (var i = 0; i < stars.length; i++)
+    {
+
+        var actPositionX = stars[i].style.left;
+        var actPositionY = stars[i].style.top;
+        actPositionX = Number(actPositionX.substring(0, actPositionX.length-2));
+        actPositionY = Number(actPositionY.substring(0, actPositionY.length-2));
+        console.log(actPositionX +","+ actPositionY);
+        console.log(getMouseX(), getMouseY());
+
+        if ((actPositionX <= getMouseX()+100 && actPositionY <= getMouseY()+100) || (actPositionX <= getMouseX()-100 && actPositionY <= getMouseY()-100)) 
+        {
+            stars[i].style.background = "yellow";    
+        }
+        else if ((actPositionY <= getMouseY()-100 && actPositionX <= getMouseX()-100) || (actPositionY <= getMouseY()+100 && actPositionX <= getMouseX()+100))
+        {
+            stars[i].style.background = "white";
+        }
+        else
+        {
+            stars[i].style.background = "white";
+        }
+    }
+   
+  }, 1000);
