4f6550e633a3775ad38d6fb2f33bd21c017467fb
luczassz <szabszika00@gmail.com>
2022-03-08 09:08:01 +0100

    valami

--- Static Html Lib/szabolcs/Stardust/stardust.js
+++ Static Html Lib/szabolcs/Stardust/stardust.js
@@ -65,18 +65,23 @@ setInterval(function() {
         }*/
         if (((actPositionY <= getMouseY()+100 && actPositionY >= getMouseY()+0) && (actPositionX <= getMouseX()+100 && actPositionX >= getMouseX()+0))) 
         {
-            stars[i].style.background = "yellow";    
+            stars[i].style.background = "blue";    
         }
         else if (((actPositionY >= getMouseY()-100 && actPositionY <= getMouseY()+0) && (actPositionX >= getMouseX()-100 && actPositionX <= getMouseX()+0))) 
         {
-            stars[i].style.background = "green";    
+            stars[i].style.background = "blue";    
         }
         //
-        else if (((actPositionY <= getMouseY()-100 && actPositionY >= getMouseY()+0) && (actPositionX >= getMouseX()-100 && actPositionX <= getMouseX()+0))) 
+        else if (((actPositionY <= getMouseY()+100 && actPositionY >= getMouseY()+0) && (actPositionX >= getMouseX()-100 && actPositionX <= getMouseX()+0))) 
         {
-            stars[i].style.background = "blue";  
+            stars[i].style.background = "blue"; 
         }
-        else{
+        else if (((actPositionY >= getMouseY()-100 && actPositionY <= getMouseY()+0) && (actPositionX <= getMouseX()+100 && actPositionX >= getMouseX()+0))) 
+        {
+            stars[i].style.background = "blue"; 
+        } 
+        else
+        {
             stars[i].style.background = "white";
         }
     }
