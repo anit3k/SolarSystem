e368ca0791f2472945aafc6ab7f62d80887712d6
Mads Tinko Madsen <mtm@itklyngen.dk>
2022-03-17 13:38:40 +0100

    Added logic handling for arduino exception.

--- SolarSystem/wwwroot/js/signal.js
+++ SolarSystem/wwwroot/js/signal.js
@@ -5,7 +5,15 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/planetSelecterHub"
 
 connection.on("PlanetSelecter", function (message) {
 
+    let planetId = parseInt(message);
+
+    if (planetId >= 4) {
+        planetId += 1;
+        message = planetId.toString();
+    }
+
     let temp = "http://10.108.149.16:81/Home/Detail/" + message;
+
     location.href = temp;
 
 });
